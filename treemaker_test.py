57dc872cd8c2b1f6ab5c849ad72d32cf3f4802f1
Ryan <ryantoh99@gmail.com>
2024-01-21 17:46:05 +0800

    feat: get_size() of node

--- treemaker_test.py
+++ treemaker_test.py
@@ -40,3 +40,16 @@ class TestTreeNode(unittest.TestCase):
             tree = TreeNode.from_treestring(test_tuple[0])
             expected_depth = test_tuple[1]
             self.assertEqual(tree.get_depth(), expected_depth)
+
+    def test_size(self):
+        test_tuples = [
+            ("A(A1(A11 A12(A121 A122 A123)) A2 A3(A31 A32 A33) A4)", 13),
+            ("A(A1(A11 A12) A2 A3(A31) A4)", 8),
+            ("A(A1(A11 A12) A2 A3(A31) A4 A5(A51 A52 A53(A531 A532(A5321) A533) A54) A6)", 18),
+            ("A(B(C(D(E(F(G(H(I(J(K(L(M(N(O(P(Q(R(S))))))))))))))))) Z Y)", 21)
+            ]
+
+        for test_tuple in test_tuples:
+            tree = TreeNode.from_treestring(test_tuple[0])
+            expected_size = test_tuple[1]
+            self.assertEqual(tree.get_size(), expected_size)
