053fbd26a19b677e45fd5c1b08a22eb16b8cfa8b
Ryan <ryantoh99@gmail.com>
2024-01-21 17:36:35 +0800

    feat: tree depth computation

--- treemaker_test.py
+++ treemaker_test.py
@@ -27,3 +27,16 @@ class TestTreeNode(unittest.TestCase):
 
         for treestr in treestrs:
             self.assertEqual(TreeNode.from_treestring(treestr).__repr__(), treestr)
+
+    def test_depth(self):
+        test_tuples = [
+            ("A(A1(A11 A12(A121 A122 A123)) A2 A3(A31 A32 A33) A4)", 4),
+            ("A(A1(A11 A12) A2 A3(A31) A4)", 3),
+            ("A(A1(A11 A12) A2 A3(A31) A4 A5(A51 A52 A53(A531 A532(A5321) A533) A54) A6)", 5),
+            ("A(B(C(D(E(F(G(H(I(J(K(L(M(N(O(P(Q(R(S))))))))))))))))) Z Y)", 19)
+            ]
+
+        for test_tuple in test_tuples:
+            tree = TreeNode.from_treestring(test_tuple[0])
+            expected_depth = test_tuple[1]
+            self.assertEqual(tree.get_depth(), expected_depth)
